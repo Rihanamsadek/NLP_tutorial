441267e18dc6c3e5c10e3549f01ae2fa7188e53e
Rihanamsadek <rayhana.msadek@esprit.tn>
2020-10-31 22:41:48 -0400

    Update Try_languages.py

--- Try_languages.py
+++ Try_languages.py
@@ -21,3 +21,45 @@ doc = nlp("Liebe Grüße!")
 
 # Print the document text
 print(doc.text)
+
+# Import the Spanish language class
+from spacy.lang.es import Spanish       
+
+# Create the nlp object
+nlp = Spanish()
+# Process a text (this is Spanish for: "How are you?")
+doc = nlp("¿Cómo estás?")
+
+# Print the document text
+print(doc.text)
+
+
+# Import the English language class and create the nlp object
+from spacy.lang.en import English
+nlp = English()
+
+# Process the text
+doc = nlp("I like tree kangaroos and narwhals.")
+
+# Select the first token 
+# We usually start with 0 instead of 1
+first_token = doc[0]
+
+# Print the first token's text
+print(first_token.text)
+
+# Import the English language class and create the nlp object
+from spacy.lang.en import English
+nlp = English()
+
+# Process the text
+doc = nlp("I like tree kangaroos and narwhals.")
+
+# A slice of the Doc for "tree kangaroos"
+tree_kangaroos = doc[2:4]
+print(tree_kangaroos.text)
+
+# A slice of the Doc for "tree kangaroos and narwhals" (without the ".")
+# Les espaces sont aussi considérées comme tokens
+tree_kangaroos_and_narwhals = doc[2:6]
+print(tree_kangaroos_and_narwhals.text)
