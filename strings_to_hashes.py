118e6ad4816b6a8909176801b1abc92d116c8933
Rihanamsadek <rayhana.msadek@esprit.tn>
2020-11-03 00:25:09 -0500

    Create strings_to_hashes.py

---
+++ strings_to_hashes.py
@@ -0,0 +1,101 @@
+# Look up the hash for the word "cat"
+cat_hash = nlp.vo
+
+# Look up the hash for the string label "PERSON"
+person_hash = nlp.vocab.strings["PERSON"]
+print(person_hash)
+
+# Look up the person_hash to get the string
+person_string = nlp.vocab.strings[person_hash]
+print(person_string)
+
+#creating a doc
+# Import the Doc class
+from spacy.tokens import Doc
+
+# Desired text: "spaCy is cool!"
+words = ['spaCy', 'is', 'cool', '!']
+spaces = [True, True, False, False]
+
+# Create a Doc from the words and spaces
+doc = Doc(nlp.vocab, words=words, spaces=spaces)
+print(doc.text)
+
+.strings["cat"]
+print(cat_hash)
+
+# Look up the cat_hash to get the string
+cat_string = nlp.vocab.strings[cat_hash]
+print(cat_string)
+
+# Look up the hash for the string label "PERSON"
+person_hash = nlp.vocab.strings["PERSON"]
+print(person_hash)
+
+# Look up the person_hash to get the string
+person_string = nlp.vocab.strings[person_hash]
+print(person_string)
+
+# Import the Doc class
+from spacy.tokens import Doc
+
+# Desired text: "Go, get started!"
+words = ['Go', ',', 'get', 'started', '!']
+spaces = [False,True,True, False,False]
+
+# Create a Doc from the words and spaces
+doc = Doc(nlp.vocab, words=words, spaces=spaces)
+print(doc.text)
+
+# Import the Doc class
+from spacy.tokens import Doc
+
+# Desired text: "Oh, really?!"
+words = ['Oh', ',', 'really','?', '!']
+spaces = [False, True, False, False, False]
+
+# Create a Doc from the words and spaces
+doc = Doc(nlp.vocab, words=words, spaces=spaces)
+print(doc.text)
+
+
+# Import the Doc and Span classes
+from spacy.tokens import Doc, Span
+
+words = ['I', 'like', 'David', 'Bowie']
+spaces = [True, True, True, False]
+
+# Create a doc from the words and spaces
+doc = Doc(nlp.vocab, words=words, spaces=spaces)
+print(doc.text)
+
+#In this exercise, you'll create the Doc and Span objects manually, and update the named entities – just like spaCy does behind the scenes. A shared nlp object has already been created
+
+# Import the Doc and Span classes
+from spacy.tokens import Doc, Span
+
+words = ['I', 'like', 'David', 'Bowie']
+spaces = [True, True, True, False]
+
+# Create a doc from the words and spaces
+doc = Doc(nlp.vocab, words=words, spaces=spaces)
+print(doc.text)
+
+
+########################################
+# Overwrite the doc.ents with a list of one entity, the "David Bowie" span. #
+########################################
+# Import the Doc and Span classes
+from spacy.tokens import Doc, Span
+
+# Create a doc from the words and spaces
+doc = Doc(nlp.vocab, words=['I', 'like', 'David', 'Bowie'], spaces=[True, True, True, False])
+
+# Create a span for "David Bowie" from the doc and assign it the label "PERSON"
+span = Span(doc, 2, 4, label='PERSON')
+
+# Add the span to the doc's entities
+doc.ents = [span]
+
+# Print entities' text and labels
+print([(ent.text, ent.label_) for ent in doc.ents])
